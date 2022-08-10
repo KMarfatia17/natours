f9a5f1fd3775b556309a9d8524f4e7211db3bcd9
KMarfatia17 <ketulm27@gmail.com>
2022-08-10 14:22:02 +0530

    section 07 error handling of course initial check in

--- after-section-06/app.js
+++ after-section-06/app.js
@@ -3,6 +3,7 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -15,11 +16,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -27,5 +23,30 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `cannot find path to ${req.originalURL} on this server!`
+  // });
+  const err = new Error(
+    `cannot find path to ${req.originalURL} on this server!`
+  );
+  err.status = 'Fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.message =
+    err.message || `cannot find path to ${req.originalURL} on this server!`;
+  err.status = err.status || 'Fail';
+
+  res.statusCode(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 module.exports = app;
