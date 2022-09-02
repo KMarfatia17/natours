5f44abcfd3f81196462719a01ca9edeaabe24c0e
KMarfatia17 <ketulm27@gmail.com>
2022-09-02 19:32:41 +0530

    feat/ security section finished

--- after-section-06/app.js
+++ after-section-06/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -8,12 +12,16 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/ErrorController');
 
 const app = express();
+// set security http headers
+app.use(helmet());
 
 // 1) Global MIDDLEWARES
+// development loging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// limit requests from same IP
 const limiter = rateLimit({
   max: 170,
   windowMs: 60 * 60 * 1000,
@@ -22,9 +30,33 @@ const limiter = rateLimit({
 
 app.use('/app', limiter);
 
-app.use(express.json());
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization will remove $ sign and dots from the operators from query string and params
+app.use(mongoSanitize());
+
+// protection from mallicious html code with javascript attached to it
+app.use(xss());
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whiteList: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
+
+// serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
