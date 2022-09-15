58483a98485e532ee5e609bab730cb5f54582c89
KMarfatia17 <ketulm27@gmail.com>
2022-09-15 18:15:26 +0530

    feat/ pug templating

--- after-section-06/app.js
+++ after-section-06/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,12 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/ErrorController');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
 // set security http headers
 app.use(helmet());
 
@@ -54,9 +61,6 @@ app.use(
   })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -64,6 +68,10 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
+app.use('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
